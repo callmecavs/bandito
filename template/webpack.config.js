2911d1e4b43c99dbaf101f684e2445ba201851e7
Michael Cavalea <callmecavs@gmail.com>
2016-11-13 22:05:33 -0500

    add plugins

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -44,6 +44,25 @@ const config = {
     ]
   },
 
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': !isProd ? '"development"' : '"production"',
+      'process.env.BROWSER': true,
+      'DEV': !isProd
+    }),
+
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+
+    ...!isProd ? [] : [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8: true
+        }
+      })
+    ]
+  ],
+
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist',
