d2d23782a6fb2f136b3c8ea1ecc4706ba76425a8
Michael Cavalea <callmecavs@gmail.com>
2016-11-13 23:12:07 -0500

    fix Main component

--- template/src/components/Main.js
+++ template/src/components/Main.js
@@ -2,11 +2,11 @@ import React, { Component, PropTypes } from 'react'
 
 class Main extends Component {
   render = () => {
-    return {
+    return (
       <div className="site">
         Hello World
       </div>
-    }
+    )
   }
 }
 
