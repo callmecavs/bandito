8d553c4944de4e6014d4e56ca62b705ad0e4bcd1
callmecavs <callmecavs@gmail.com>
2016-11-19 19:18:16 -0500

    include reset/base in Main, fix Main stylesheet

---
+++ template/src/components/Main/Main.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react'
+
+import '../../config/reset.scss'
+import '../../config/base.scss'
+
+import './main.scss'
+
+class Main extends Component {
+  render = () => {
+    return (
+      <div className="site">
+        outset-webpack
+      </div>
+    )
+  }
+}
+
+export default Main
